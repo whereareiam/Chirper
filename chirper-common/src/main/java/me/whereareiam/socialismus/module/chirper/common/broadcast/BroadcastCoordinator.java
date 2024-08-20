b9e1591eaf9a4e85e70dc33d90f9c59afc352a04
whereareiam <mmm.g032@gmail.com>
2024-08-20 12:07:32 +0200

    chore: update to latest api version.

--- chirper-common/src/main/java/me/whereareiam/socialismus/module/chirper/common/broadcast/BroadcastCoordinator.java
+++ chirper-common/src/main/java/me/whereareiam/socialismus/module/chirper/common/broadcast/BroadcastCoordinator.java
@@ -2,9 +2,9 @@ package me.whereareiam.socialismus.module.chirper.common.broadcast;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import me.whereareiam.socialismus.api.input.container.PlayerContainerService;
 import me.whereareiam.socialismus.api.model.player.DummyPlayer;
 import me.whereareiam.socialismus.api.model.scheduler.DelayedRunnableTask;
-import me.whereareiam.socialismus.api.output.PlatformInteractor;
 import me.whereareiam.socialismus.api.output.Scheduler;
 import me.whereareiam.socialismus.module.chirper.api.input.AnnouncementBroadcaster;
 import me.whereareiam.socialismus.module.chirper.api.model.announcement.Announcement;
@@ -12,36 +12,39 @@ import me.whereareiam.socialismus.module.chirper.api.model.announcement.Announce
 import me.whereareiam.socialismus.module.chirper.api.type.AnnouncementType;
 import me.whereareiam.socialismus.module.chirper.common.requirement.RequirementValidator;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Singleton
 public class BroadcastCoordinator implements AnnouncementBroadcaster {
     private final Scheduler scheduler;
-    private final PlatformInteractor interactor;
+    private final PlayerContainerService playerContainer;
     private final BroadcastSender broadcastSender;
     private final RequirementValidator requirementValidator;
 
     @Inject
-    public BroadcastCoordinator(Scheduler scheduler, PlatformInteractor interactor, BroadcastSender broadcastSender, RequirementValidator requirementValidator) {
+    public BroadcastCoordinator(Scheduler scheduler, PlayerContainerService playerContainer, BroadcastSender broadcastSender, RequirementValidator requirementValidator) {
         this.scheduler = scheduler;
-        this.interactor = interactor;
+        this.playerContainer = playerContainer;
         this.broadcastSender = broadcastSender;
         this.requirementValidator = requirementValidator;
     }
 
     @Override
     public void broadcast(Announcement announcement) {
-        List<DummyPlayer> recipients = interactor.getOnlinePlayers();
+        final Random random = new Random();
+
+        Set<DummyPlayer> recipients = playerContainer.getPlayers();
         recipients = recipients.parallelStream()
                 .filter(r -> requirementValidator.checkRequirements(announcement, r))
-                .toList();
+                .collect(Collectors.toSet());
 
         if (announcement.getSettings().getDelay() > 0) {
-            List<DummyPlayer> finalRecipients = recipients;
+            Set<DummyPlayer> finalRecipients = recipients;
             scheduler.schedule(DelayedRunnableTask.builder()
-                    .id(new Random().nextInt())
+                    .id(random.nextInt())
                     .module("chirper")
                     .delay(announcement.getSettings().getDelay() * 1000L)
                     .runnable(() -> sendAnnouncement(announcement, finalRecipients))
@@ -60,11 +63,11 @@ public class BroadcastCoordinator implements AnnouncementBroadcaster {
             return;
         }
 
-        List<DummyPlayer> recipients = interactor.getOnlinePlayers();
+        Set<DummyPlayer> recipients = playerContainer.getPlayers();
         sendAnnouncement(announcement, recipients);
     }
 
-    private void sendAnnouncement(Announcement announcement, List<DummyPlayer> recipients) {
+    private void sendAnnouncement(Announcement announcement, Set<DummyPlayer> recipients) {
         for (DummyPlayer recipient : recipients) {
             for (Map.Entry<AnnouncementType, ? extends AnnouncementContent> entry : announcement.getContents().entrySet())
                 if (requirementValidator.checkRequirements(entry.getValue(), recipient))
